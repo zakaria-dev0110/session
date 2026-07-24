4093c27f5152c26a94adc88ff9c3950a52247666
Muhammed Zakaria Windows <zakaria.dev0110@gmail.com>
2026-07-24 23:21:17 +0300

    Successfully created a session and added and displayed items in array during the session

--- index.php
+++ index.php
@@ -1,3 +1,26 @@
-<?php
-    session_start();
-?>
+<!DOCTYPE html>
+<html>
+        <?php
+            session_start();
+            if (!isset($_SESSION['inputs'])) {
+                $_SESSION['inputs'] = array();
+            }
+            if ($_SERVER["REQUEST_METHOD"] == 'POST') {
+                array_push($_SESSION['inputs'], $_POST['name']);
+            }
+        ?>
+
+
+    <form method="POST">
+        <input type="text" name="name" required/>
+        <input type="submit" value="Submit"/>
+    </form>
+    <h1>
+        <?php
+        echo "<pre>";
+        print_r($_SESSION['inputs']);
+        echo "</pre>";
+        ?>
+    </h1>
+</html>
+
